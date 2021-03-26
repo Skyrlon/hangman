1cd5521a38484ec1638d366f5e863b93854a3fe3
Antoine Drouault <antoinedrouault@hotmail.com>
2021-03-26 18:48:28 +0100

    WordToGuess show only letters in lettersPressed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,31 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    lettersPressed: ['a', 'n'],
+    alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  }
+
+  handleKeyDown = (e) => {
+    const { lettersPressed, alphabet } = this.state
+    let array = lettersPressed
+    if (alphabet.includes(e.key) && !array.includes(e.key)) {
+      array.push(e.key)
+      this.setState({ lettersPressed: array })
+      console.log(e.key)
+      console.log(lettersPressed)
+    }
+  }
+
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+
   render() {
     return (
       <div className="App" >
@@ -14,7 +39,7 @@ class App extends Component {
             <Hangman errorNumber={6} />
           </div>
         </div>
-        <WordToGuess />
+        <WordToGuess lettersProposed={this.state.lettersPressed} />
       </div>
     )
   }
--- src/WordToGuess.js
+++ src/WordToGuess.js
@@ -1,13 +1,12 @@
 const wordToGuess = 'Hangman';
 const lettersOfWordToGuess = wordToGuess.split('');
 
+const WordToGuess = ({ lettersProposed }) => (
+    <div className="word-to-guess">
+        {lettersOfWordToGuess.map((letter, index) => lettersProposed.includes(letter) ? <div key={index}>{letter}</div> : <div key={index}>{'_'}</div>)}
+    </div>
+)
 
-const WordToGuess = () => {
-    return (
-        <div className="word-to-guess">
-            {lettersOfWordToGuess.map((letter, index) => <div key={index}>{letter}</div>)}
-        </div>
-    )
-}
+
 
 export default WordToGuess;
