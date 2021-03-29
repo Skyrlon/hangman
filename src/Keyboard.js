d8eba6673387a75c89273eb79db1cfa90a6737c2
Antoine Drouault <antoinedrouault@hotmail.com>
2021-03-29 23:14:45 +0200

    added keyboard in alphabetic order, changed keyboards style

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,5 +1,6 @@
 const azerty = 'azertyuiopqsdfghjklmwxcvbn'.split('')
 const qwerty = 'qwertyuiopasdfghjklzxcvbnm'.split('')
+const abcdef = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
 
 
@@ -7,10 +8,37 @@ const Keyboard = ({ sendLetter }) => {
     return (
         <div className='keyboard' >
             <div className="azerty">
-                {azerty.map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                <div className="row one">
+                    {azerty.slice(0, 11).map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                </div>
+                <div className="row two">
+                    {azerty.slice(11, 20).map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                </div>
+                <div className="row three">
+                    {azerty.slice(20).map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                </div>
             </div>
             <div className="qwerty">
-                {qwerty.map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                <div className="row one">
+                    {qwerty.slice(0, 10).map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                </div>
+                <div className="row two">
+                    {qwerty.slice(10, 19).map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                </div>
+                <div className="row three">
+                    {qwerty.slice(19).map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                </div>
+            </div>
+            <div className="abcdef">
+                <div className="row one">
+                    {abcdef.slice(0, 11).map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                </div>
+                <div className="row two">
+                    {abcdef.slice(11, 20).map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                </div>
+                <div className="row three">
+                    {abcdef.slice(20).map((letter, index) => <div onClick={() => sendLetter(letter)} key={index}>{letter}</div>)}
+                </div>
             </div>
         </div >
     )
